afd7629560cb54948ea140e99cb8d0480fe9ea0c
John Doe <tristan.varciat@gmail.com>
2020-11-24 12:57:24 +0100

    Start

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import logo from './pere.jpg';
+import './App.css';
+        
+function App() {
+  return (
+    
+    <div className="App">
+       <script src="http://images.topflood.com/neige-maric.php"  type="text/javascript"></script>
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <script src="https://www.lauyan.com/goodies/snow/snow.js?8" id="_x2"></script>
+
+        <p>
+       
+         Ma liste pour le Père-Noël :
+        </p>
+        <a
+          className="App-link"
+          href="https://www.mercedes-benz.fr/passengercars/mercedes-benz-cars/models/a-class/hatchback-w177/amg.html"
+        >
+              Une AMG a45
+        </a>
+        <a
+          className="App-link"
+          href="https://i.pinimg.com/originals/d0/b5/3e/d0b53e6af3ffff6422691abe08a8e1a2.jpg"
+        >
+          Un nouveau partenaire
+        </a>
+
+        <a
+          className="App-link"
+          href="https://www.faux-diplome.org/wp-content/uploads/2018/02/Licence-Pro.jpg"
+        >
+          Une bonne note en react
+        </a>
+
+        <a
+          className="App-link"
+          href="https://i1.wp.com/imagesociale.fr/wp-content/uploads/Raoult_Gandalf.jpg?fit=1198%2C1060&ssl=1"
+        >
+          Un vaccin contre la Covid19
+        </a>
+
+        <a
+          className="App-link"
+          href="https://i.pinimg.com/originals/a1/db/e4/a1dbe46bd6a0c64b0c6e2cf1af9f8a74.jpg"
+        >
+          Une (vraie) dernière saison de Game of Thrones
+        </a>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
